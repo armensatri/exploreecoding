e1936e482692244641688f600dbab252ece531e5
armensatri <belajarcoding444@gmail.com>
2025-04-02 23:51:54 +0700

    update

--- resources/views/frontend/template/header.blade.php
+++ resources/views/frontend/template/header.blade.php
@@ -66,14 +66,20 @@
         </div>
 
         <div id="dropdownAuth"
-          class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+          class="z-10 hidden w-48 bg-white border border-gray-400 divide-y divide-gray-100 shadow rounded-3xl">
 
           <ul aria-labelledby="dropdownHoverButton"
             class="py-2 text-sm text-gray-700 dark:text-gray-200">
             <li>
-              <x-route-to-dashboard
-                :route="\App\Helpers\Redirects::Dashboard()"
-              />
+              <div class="p-3 dashboard">
+                <span class="block px-3 py-2 text-sm font-medium tracking-wider text-green-700 uppercase">
+                  BACK TO
+                </span>
+
+                <x-route-to-dashboard
+                  :route="route(\App\Helpers\Redirects::Dashboard())"
+                />
+              </div>
             </li>
 
             <form action="{{ route('logout') }}"
@@ -81,10 +87,10 @@
               @csrf
 
               <button type="submit"
-                class="px-4 py-1 mb-3 bg-red-600 hover:bg-red-600 rounded-xl">
+                class="px-3 py-1 mt-4 mb-6 bg-red-600 shadow-sm hover:bg-red-700 rounded-xl">
                 <div class="inline-flex items-center justify-center">
                   <i class="text-white bi bi-arrow-right-circle"></i>
-                  <span class="ml-1 text-sm tracking-wider text-white">
+                  <span class="ml-1 text-base tracking-wider text-white">
                     Logout
                   </span>
                 </div>
@@ -111,16 +117,87 @@
     x-transition:leave-end="opacity-0"
     class="lg:hidden" role="dialog" aria-modal="true">
     <div class="fixed inset-0 z-50"></div>
-    <div class="fixed inset-y-0 right-0 z-50 w-full px-6 py-6 overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-
+    <div class="fixed inset-y-0 right-0 z-50 w-full px-6 py-5 overflow-y-auto rounded-tl-3xl bg-sky-50 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div class="flex items-center justify-between">
-        <a href="{{ route('home') }}"
+        <div>
+          @auth
+            <div id="dropdownHoverButtonn"
+              data-dropdown-toggle="dropdownAuthh"
+              data-dropdown-trigger="hover"
+              class="flex items-center gap-x-2 tracking-wide justify-center py-[3px] text-base font-medium rounded-[13px] cursor-pointer">
+
+              <picture>
+                <img src="{{ Auth::user() && Auth::user()->image ?
+                  asset('storage/' . Auth::user()->image) :
+                  '/backend/img/user.png' }}"
+                  alt="user-default"
+                  loading="lazy"
+                  class="object-cover object-top p-0.5
+                  bg-white rounded-full w-9 h-9"
+                />
+              </picture>
+
+              <span class="text-[17px] font-normal tracking-wide text-black truncate sm:block">
+                <span>@</span>{{ Auth::user()->username }}
+              </span>
+
+              <i class="text-base text-black bi bi-arrow-down-circle"></i>
+            </div>
+
+            <div id="dropdownAuthh"
+              class="z-10 hidden w-48 bg-white border border-gray-400 divide-y divide-gray-100 shadow rounded-3xl">
+
+              <ul aria-labelledby="dropdownHoverButtonn"
+                class="py-2 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                  <div class="p-3 dashboard">
+                    <span class="block px-3 py-2 text-sm font-medium tracking-wider text-green-700 uppercase">
+                      BACK TO
+                    </span>
+
+                    <x-route-to-dashboard
+                      :route="route(\App\Helpers\Redirects::Dashboard())"
+                    />
+                  </div>
+                </li>
+
+                <form action="{{ route('logout') }}"
+                  method="POST" class="flex justify-center">
+                  @csrf
+
+                  <button type="submit"
+                    class="px-3 py-1 mt-4 mb-6 bg-red-600 shadow-sm hover:bg-red-700 rounded-xl">
+                    <div class="inline-flex items-center justify-center">
+                      <i class="text-white bi bi-arrow-right-circle"></i>
+                      <span class="ml-1 text-base tracking-wider text-white">
+                        Logout
+                      </span>
+                    </div>
+                  </button>
+                </form>
+              </ul>
+            </div>
+          @else
+            <a href="{{ route('login') }}"
+              class="px-3.5 mr-2.5 py-1 text-base font-semibold leading-6 text-gray-900 bg-blue-200 border border-gray-400 rounded-xl
+              hover:bg-blue-300">
+              Login
+            </a>
+
+            <a href="{{ route('register') }}"
+              class="px-3.5 py-1 text-base font-semibold leading-6 text-gray-900 bg-blue-200 border border-gray-400 rounded-xl
+              hover:bg-blue-300">
+              Register
+            </a>
+          @endauth
+        </div>
+        {{-- <a href="{{ route('home') }}"
           class="-m-1.5 p-1.5">
           <img src="/frontend/img/logo.jpg"
             alt="logo"
             class="w-auto h-8 rounded-full"
           />
-        </a>
+        </a> --}}
 
         <button @click="openSidebar = false"
           type="button"
@@ -130,7 +207,7 @@
             stroke-width="1.5"
             stroke="currentColor"
             aria-hidden="true"
-            class="w-6 h-6">
+            class="p-1 rounded-full w-7 h-7 hover:bg-red-500 hover:text-white hover:border hover:border-gray-600">
             <path stroke-linecap="round"
               stroke-linejoin="round"
               d="M6 18L18 6M6 6l12 12"
