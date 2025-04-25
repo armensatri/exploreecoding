4c4f18343e310dc1a64e01b627b6c17d7f9aeb4b
armensatri <belajarcoding444@gmail.com>
2025-04-25 16:01:40 +0700

    update

--- app/Providers/FrontendServiceProvider.php
+++ app/Providers/FrontendServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use App\View\Components\Frontend\Header\RouteToDashboard;
 
+use App\View\Components\Frontend\MenuWeb\{
+  ExploreCoding,
+  MenuUtama,
+  ExploreLainnya,
+  MenuWebHeader,
+};
+
 class FrontendServiceProvider extends ServiceProvider
 {
   public function register(): void
@@ -17,5 +24,11 @@ class FrontendServiceProvider extends ServiceProvider
   {
     // HEADER
     Blade::component('route-to-dashboard', RouteToDashboard::class);
+
+    // MENU WEB
+    Blade::component('explore-coding', ExploreCoding::class);
+    Blade::component('menu-web-header', MenuWebHeader::class);
+    Blade::component('menu-utama', MenuUtama::class);
+    Blade::component('explore-lainnya', ExploreLainnya::class);
   }
 }
--- resources/views/frontend/template/_temp/menu-web.blade.php
+++ resources/views/frontend/template/_temp/menu-web.blade.php
@@ -25,177 +25,102 @@
         class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 relative w-full min-w-60 hidden z-10 top-full start-0 before:absolute before:-top-5 before:start-0 before:w-full before:h-5" >
 
         <div class="bg-white md:mx-6 lg:mx-8 md:rounded-3xl">
-          <div class="gap-4 py-1 md:p-4 md:grid md:grid-cols-2 lg:grid-cols-3">
+          <div
+            class="gap-4 py-1 md:p-4 md:grid md:grid-cols-2 lg:grid-cols-3">
             <div class="bg-sky-100 rounded-2xl">
               <div class="flex flex-col p-2">
-                <span
-                  class="ms-2.5 mb-4 tracking-wide font-bold text-base uppercase text-gray-800 dark:text-neutral-200
-                  text-center mt-4">
-                  explore coding
-                </span>
-
-                <div class="inline-flex justify-center">
-                  <img src="/image/default.png"
-                    alt="logo"
-                    class="size-32"
-                  />
-                </div>
-
-                <div class="mb-3 text-center grow">
-                  <div class="p-4 text-sm font-medium text-gray-700">
-                    Belajar programming online dengan materi terstruktur untuk kuasai coding dari dasar hingga mahir.
-                  </div>
-
-                  <a href="">
-                    <button
-                      class="text-sm font-medium bg-blue-200 hover:bg-blue-300 text-black px-2.5 py-1 rounded-xl border border-gray-400 dark:text-neutral-200">
-                      belajar sekarang
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div class="bg-sky-100 rounded-l-2xl">
-              <div class="flex flex-col p-2 h-[320px] overflow-y-scroll">
-                <span
-                  class="ms-2.5 mb-4 tracking-wide font-bold text-base uppercase text-gray-800 dark:text-neutral-200
-                  text-center mt-4">
-                  menu utama
-                </span>
-
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <img src="/frontend/img/fe-home.png"
-                    alt="fe-menu"
-                    class="size-6"
-                  />
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Home</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Bangun skill coding mu dengan belajar di exploreecoding
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <img src="/frontend/img/fe-about.png"
-                    alt="fe-about"
-                    class="size-6"
-                  />
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">About</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Tentang coding dan perjalanan membangun platform ini
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <img src="/frontend/img/fe-materi.png"
-                  alt="fe-about"
-                  class="size-6"
+                <x-explore-coding
+                  header="explore coding"
+                  logo="/image/default.png"
+                  description="Belajar programming online dengan materi terstruktur untuk kuasai coding dari dasar hingga mahir."
+                  route="#"
                 />
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Materi</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Materi terdiri dari path, roadmap, playlist, dan post, yang akan terus di update
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Sitemap</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Panduan lengkap untuk menelusuri semua konten situs
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Contact</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Hubungi kami untuk pertanyaan, saran, atau kerja sama
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Leaderboard</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Pengguna teratas dengan progres belajar terbaik saat ini
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Update News</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Update rutin tentang materi baru dan pengumuman penting
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Request Tutorial</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Punya ide tutorial ? Kirim permintaanmu sekarang juga
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Privacy Policy</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Informasi lengkap tentang hak dan privasi pengunjung situs
-                    </div>
-                  </div>
-                </a>
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">Update Logs</div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Catatan perubahan dan pembaruan platform yang terbaru
-                    </div>
-                  </div>
-                </a>
               </div>
             </div>
 
             <div class="bg-sky-100 rounded-l-2xl">
               <div class="flex flex-col p-2 h-[320px] overflow-y-scroll">
-                <span
-                  class="ms-2.5 mb-4 tracking-wide font-bold text-base uppercase text-gray-800 dark:text-neutral-200
-                  text-center mt-4">
-                  explore lainnya
-                </span>
+                <x-menu-web-header
+                  name="menu utama"
+                />
 
-                <a href="#"
-                  class="flex px-5 py-3 rounded-2xl gap-x-4 hover:bg-gradient-to-r hover:from-red-200 hover:to-green-200">
-                  <i class="bi bi-house"></i>
-                  <div class="grow">
-                    <div class="text-base font-semibold text-gray-800 dark:text-neutral-200 tracking-wide">
-                      App Project
-                    </div>
-                    <div class="text-sm font-medium text-gray-600 dark:text-neutral-500">
-                      Ikuti project aplikasi kami untuk pengalaman coding nyata
-                    </div>
-                  </div>
-                </a>
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="HOME"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-about.png"
+                  menu="ABOUT"
+                  description="Tentang coding dan perjalanan membangun platform ini"
+                />
+
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+                <x-menu-utama
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="home"
+                  description="Bangun skill coding mu dengan belajar di exploreecoding"
+                />
+              </div>
+            </div>
+
+            <div class="bg-sky-100 rounded-l-2xl">
+              <div class="flex flex-col p-2 h-[320px] overflow-y-scroll">
+                <x-menu-web-header
+                  name="explore lainnya"
+                />
+
+                <x-explore-lainnya
+                  route="#"
+                  image="/frontend/img/fe-home.png"
+                  menu="APP PROJECT"
+                  description="Ikuti project aplikasi kami untuk pengalaman coding nyata"
+                />
               </div>
             </div>
           </div>
