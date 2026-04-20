2e30b593cf290cf804e1dae2e7d2484493399a7f
armensatri <belajarcoding444@gmail.com>
2026-04-20 10:23:33 +0700

    update

--- app/Http/Controllers/Backend/Managemenu/SubmenusController.php
+++ app/Http/Controllers/Backend/Managemenu/SubmenusController.php
@@ -8,59 +8,59 @@ use Illuminate\Http\Request;
 
 class SubmenusController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    // ini lagi ya
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Submenu $submenu)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Submenu $submenu)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Submenu $submenu)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Submenu $submenu)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Submenu $submenu)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Submenu $submenu)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Submenu $submenu)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Submenu $submenu)
+  {
+    //
+  }
 }
